5891060ff352271fd139c7e09282ee23405c5089
totsamiyznakomiy <totsamiyznakomiy@gmail.com>
2023-05-01 23:49:17 +0700

    добавил меню)))

--- src/App.js
+++ src/App.js
@@ -1,10 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import Food from './componets/menu';
 
 function App() {
+  let foods = [
+    {
+      name: "Chocolate chip cookies",
+      ingredients: [
+        "flour",
+        "sugar",
+        "butter",
+        "eggs",
+        "salt"
+      ],
+      sellingPrice: 29,
+    },
+    {
+      name: "Spaghetti with meatballs",
+      ingredients: [
+        "pasta",
+        "groundBeef",
+        "tomatoSauce",
+        "breadCrumbs",
+        "eggs"
+      ],
+      sellingPrice: 28,
+    },
+    {
+      name: "Chicken stir-fry",
+      ingredients: [
+        "chickenBreast",
+        "vegetables",
+        "soySauce",
+        "cornstarch",
+        "rice"
+      ],
+      sellingPrice: 15,
+    }
+  ]
   return (
     <div className="App">
       <header className="App-header">
+        {foods.map((item) => (
+          <Food name={item.name} ingredients={item.ingredients} sellingPrice={item.sellingPrice} />
+        ))}
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
