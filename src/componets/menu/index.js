50bdccf62fceb5c4c3a93bb2dae4a59213a9a650
totsamiyznakomiy <totsamiyznakomiy@gmail.com>
2023-05-03 01:24:31 +0700

    карточки с корзиной

--- src/componets/menu/index.js
+++ src/componets/menu/index.js
@@ -1,9 +1,51 @@
-const Food = ({ name, ingredients, sellingPrice }) => {
-    return (<>
-        <n> {name} </n>
-        <i>{ingredients} </i>
-        <s>{sellingPrice}</s>
-    </>);
+import MenuCard from "./MenuCard";
+
+const Menu = ({ onItemBuy }) => {
+    let dishes = [
+        {
+            name: "Chocolate chip cookies",
+            sellingPrice: 100,
+            ingredients: [
+                "flour",
+                "sugar",
+                "butter",
+                "eggs",
+                "salt"
+            ],
+
+            image: "/jpeg/chockolate.jpg",
+        },
+        {
+            name: "Spaghetti with meatballs",
+            sellingPrice: 300,
+            ingredients: [
+                "pasta",
+                "groundBeef",
+                "tomatoSauce",
+                "breadCrumbs",
+                "eggs"
+            ],
+            image: "/jpeg/spaghetti.jpeg",
+        },
+        {
+            name: "Chicken stir-fry",
+            sellingPrice: 200,
+            ingredients: [
+                "chickenBreast",
+                "vegetables",
+                "soySauce",
+                "cornstarch",
+                "rice"
+            ],
+            image: "/jpeg/chiken.jpg",
+        }
+    ]
+
+    return (
+        <div classname="mx-auto grid gap-2 grid-cols-2 md:grid-cols-3 xl:grid-cols-4 container">
+            {dishes.map((item) =>
+                (<MenuCard item={item} onClickBuy={onItemBuy} />))}
+        </div>);
 };
 
-export default Food;
+export default Menu;
