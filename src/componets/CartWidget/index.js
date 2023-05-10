30547dbf5fb245258dd7fd749d5fa5b02c90dcb6
totsamiyznakomiy <totsamiyznakomiy@gmail.com>
2023-05-10 21:44:06 +0700

    сделал все как Арсалана

--- src/componets/CartWidget/index.js
+++ src/componets/CartWidget/index.js
@@ -8,7 +8,7 @@ const CartWidget = ({ cart }) => {
           <CartIcon /> X {cart.length}
         </div>
         <div>
-          Сумма: {cart.reduce((sum, item) => (sum += item.price), 0)} руб
+          Sum: {cart.reduce((sum, item) => (sum += item.price), 0)} руб
         </div>
       </div>
     </div>
--- src/componets/MenuCard/index.js
+++ src/componets/MenuCard/index.js
@@ -1,25 +1,32 @@
 import { Link } from "react-router-dom";
 import style from "./MenuCard.module.css";
 
-const MenuCard = ({ item, onClickBuy = () => { } }) => {
-    const content = <div className="text-sm"></div>
-    return (
-        <div className={"bg-white rounded-xl border-4 border-black overflow-hidden shadow-lg" + style.card}>
-            <img src={item.image} className="w-full" />
-            <div className="px-2">
-                <Link to={`/catalog/${item.id}`}>
-                    <div className="text-2xl font-bold">{item.name}</div>
-                </Link>
-            </div>
+const MenuCard = ({ item, onClickBuy = () => {} }) => {
+  const ingredients = (
+    <div className="text-xs">{item.ingredients.join(", ")}</div>
+  );
 
-            <div className="p-2">{item.content.join("/ ")}</div>
-            <button
-                onClick={() => onClickBuy(item)}
-                className="w-full text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-black-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
-                Buy
-            </button>
-
-        </div>
-    );
+  return (
+    <div
+      className={
+        "rounded overflow-hidden shadow-lg dark:bg-slate-900 " + style.card
+      }
+    >
+      <img src={item.image} className="w-full" />
+      <div className="p-2">
+        <Link to={`/catalog/${item.id}`}>
+          <div className={"text-2xl font-bold " + style.title}>{item.name}</div>
+        </Link>
+        <div className="py-2">{ingredients}</div>
+        <button
+          onClick={() => onClickBuy(item)}
+          className="w-full bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+        >
+          Купить
+        </button>
+      </div>
+    </div>
+  );
 };
+
 export default MenuCard;
